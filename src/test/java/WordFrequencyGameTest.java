cd7c6a066c8043eae10486851dd7feeef24d2597
tianrui <haitongjie@qq.com>
2020-07-27 20:50:15 +0800

    [Ray] refactor: remove throw exception statement

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WordFrequencyGameTest {
 
     @Test
-    public void should_get_the_1_when_input_the() throws Exception {
+    public void should_get_the_1_when_input_the() {
         //Given
         String inputStr = "the";
         String expectResult = "the 1";
@@ -13,7 +13,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words() throws Exception {
+    public void should_process_two_words() {
         //Given
         String inputStr = "the is";
         String expectResult = "the 1\nis 1";
@@ -21,7 +21,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_spaces() throws Exception {
+    public void should_process_two_words_with_special_spaces() {
         //Given
         String inputStr = "the      is";
         String expectResult = "the 1\nis 1";
@@ -29,7 +29,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_enter() throws Exception {
+    public void should_process_two_words_with_special_enter() {
         //Given
         String inputStr = "the   \n   is";
         String expectResult = "the 1\nis 1";
@@ -37,7 +37,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_same_words_with_sorted() throws Exception {
+    public void should_process_two_same_words_with_sorted() {
         //Given
         String inputStr = "the the is";
         String expectResult = "the 2\nis 1";
@@ -45,7 +45,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_sorted_with_count_descending() throws Exception {
+    public void should_process_sorted_with_count_descending() {
         //Given
         String inputStr = "the is is";
         String expectResult = "is 2\nthe 1";
@@ -58,5 +58,6 @@ public class WordFrequencyGameTest {
         String result = game.getResult(inputStr);
         //Then
         assertEquals(result, expectResult);
+
     }
 }
